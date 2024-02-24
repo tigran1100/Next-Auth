3d784b9f20fdfce810cb2daa816763835daacee1
Tigran <tigran.balayan.y@gmail.com>
2024-02-24 16:20:32 +0400

    Added and configured prisma

--- schemas/auth.tsx
+++ schemas/auth.tsx
@@ -3,22 +3,44 @@ import { z } from "zod";
 export const loginSchema = z.object({
 	usernameOrEmail: z
 		.string()
-		.min(3, "Username or email must be at lease 4 characters long"),
-	password: z.string().min(1, "Password is required"),
+		.min(3, "Username or email must be at lease 4 characters long")
+		.max(65535, "Username or email can't be longer than 65,535 characters"),
+	password: z
+		.string()
+		.min(1, "Password is required")
+		.max(65535, "Username or email can't be longer than 65,535 characters"),
 });
 
 export const registerSchema = z
 	.object({
-		name: z.string().min(2, "Name must be at lease 2 characters long"),
+		name: z
+			.string()
+			.min(2, "Name must be at lease 2 characters long")
+			.max(
+				65535,
+				"Username or email can't be longer than 65,535 characters"
+			),
 		usernameOrEmail: z
 			.string()
-			.min(3, "Username or email must be at lease 4 characters long"),
+			.min(3, "Username or email must be at lease 4 characters long")
+			.max(
+				65535,
+				"Username or email can't be longer than 65,535 characters"
+			),
 		password: z
 			.string()
-			.min(6, "Password must be at lease 6 characters long"),
+			.min(6, "Password must be at lease 6 characters long")
+			.max(
+				65535,
+				"Username or email can't be longer than 65,535 characters"
+			),
 		passwordConfirm: z
 			.string()
-			.min(6, "Password must be at lease 6 characters long"),
+			.min(6, "Password must be at lease 6 characters long")
+			.max(
+				65535,
+				"Username or email can't be longer than 65,535 characters"
+			),
 	})
 	.superRefine(({ passwordConfirm, password }, ctx) => {
 		if (passwordConfirm !== password) {
