f837f45337348bc3266b76be8a8ce5965ad4f81c
Tigran <tigran.balayan.y@gmail.com>
2024-02-25 22:47:51 +0400

    Added NextAuth and configured middleware

---
+++ auth.ts
@@ -0,0 +1,16 @@
+// Next Auth
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+// Prisma
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/prisma/client";
+
+export const {
+	handlers: { GET, POST },
+	auth,
+} = NextAuth({
+	adapter: PrismaAdapter(prisma),
+	session: { strategy: "jwt" },
+	...authConfig,
+});
--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,39 @@
+// NextJS
 import { NextRequest, NextResponse } from "next/server";
-import {
-	updateAuthCookie,
-	setCookie,
-	setCookieFromResponse,
-	setJWTCookieFromResponse,
-} from "@/app/_utils/lib";
 
-export async function middleware(request: NextRequest) {
+// AuthJS
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+// Routes
+const publicRoutes = ["/", "/auth/login", "/auth/register"];
+const apiAuthPrefix = "/api/auth";
+
+// AuthJS
+const { auth } = NextAuth(authConfig);
+
+export default auth(async (req) => {
+	const session = req.auth;
+	const pathname = req.nextUrl.pathname;
+	let modifiableResponse = NextResponse.next();
+
 	// Redirections
-	if (request.nextUrl.pathname === "/auth") {
-		return NextResponse.redirect(new URL("/auth/login", request.url));
+	if (pathname === "/auth") {
+		return NextResponse.redirect(new URL("/auth/login", req.url));
 	}
 
-	// Other
-	let response = NextResponse.next();
-	response = (
-		await updateAuthCookie(request as NextRequest, response as NextResponse)
-	).response;
+	if (
+		!session &&
+		!publicRoutes.includes(pathname) &&
+		!pathname.startsWith(apiAuthPrefix)
+	) {
+		return NextResponse.redirect(new URL("/auth/login", req.url));
+	}
 
-	return response;
-}
+	// Returning response
+	return modifiableResponse;
+});
 
 export const config = {
-	matcher: ["/auth", "/auth/signin"],
+	matcher: ["/((?!_next/image|_next/static|favicon.ico).*)", "/"],
 };
